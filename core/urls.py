ef85128d6488a7d3a10ef65d099394a4764bbb73
David-Angel-M <110673903+David-Angel-M@users.noreply.github.com>
2023-07-02 12:33:37 -0500

    RepathUrl

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,18 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.views.static import serve 
+from django.urls import re_path
 from . import views
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include('post.urls')),
     path('',views.apiMsg)
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
