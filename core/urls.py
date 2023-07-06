fe9608b9695407d59fbe8a75502a9ead36619a2f
David-Angel-M <110673903+David-Angel-M@users.noreply.github.com>
2023-07-06 19:48:02 +0200

    token

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve 
 from django.urls import re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from . import views
 from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/affiliate/', include('user.urls')),
     path('v1/', include('post.urls')),
+    path('v1/login/', TokenObtainPairView.as_view()),
+    path('v1/refresh/', TokenRefreshView.as_view()),
+    path('v1/verify/', TokenVerifyView.as_view()),
     path('',views.apiMsg)
 ]
 
