51a9e5d37031a80af7a2e905e26c9ab8bf8f8f19
David-Angel-M <110673903+David-Angel-M@users.noreply.github.com>
2023-07-06 19:48:39 +0200

    token-post

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import PostListProv, PostCreate, PostDetailProv, PostDeleteProv, CategoryList
+from .views import PostList, PostDetail, CategoryList, PostListProv, PostCreateProv, PostDetailProv, PostDeleteProv
+from .views import PostCreate, PostDelete
 
 urlpatterns = [
-    path('post-list/', PostListProv.as_view()),
-    path('post/', PostCreate.as_view()),
-    path('post/<id>/', PostDetailProv.as_view()),
-    path('post-delete/<id>/', PostDeleteProv.as_view()),
+    path('posts-list/', PostList.as_view()),
+    path('posts/', PostCreate.as_view()),
+    path('posts/<id>/', PostDetail.as_view()),
+    path('posts-delete/<id>/', PostDelete.as_view()),
     path('category-list/', CategoryList.as_view()),
+    path('affiliate/posts-list/', PostListProv.as_view()),
+    path('affiliate/posts/', PostCreateProv.as_view()),
+    path('affiliate/posts/<id>/', PostDetailProv.as_view()),
+    path('affiliate/posts-delete/<id>/', PostDeleteProv.as_view()),
 ]
--- post/views.py
+++ post/views.py
@@ -1,27 +1,109 @@
-from rest_framework.generics import ListAPIView, DestroyAPIView, CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import ListAPIView, DestroyAPIView, RetrieveAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.views import APIView
 from .serializers import PostListSerializer, PostSerializer, CategoryListSerializer
 from .models import Post, Category
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.conf import settings
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class PostListProv(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostListSerializer
-    permission_classes = ()
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Post.objects.all()
+        else:
+            user_id = self.request.user.id
+            return Post.objects.filter(user_id=user_id)
 
 
-class PostCreate(CreateAPIView):
-    serializer_class = PostSerializer
-    permission_classes = ()
+class PostCreateProv(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+
+        if request.user.is_verified:
+            if serializer.is_valid():
+                serializer.save(user=self.request.user)
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
 
-class PostDetailProv(RetrieveUpdateAPIView):
+class PostDetailProv(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+
+    def get(self, request, id):
+        post = self.get_object(id)
+        if self.has_object_permission(request, post):
+            serializer = PostSerializer(post)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def put(self, request, id):
+        post = self.get_object(id)
+        if self.has_object_permission(request, post):
+            serializer = PostSerializer(post, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def get_object(self, id):
+        try:
+            return Post.objects.get(id=id)
+        except Post.DoesNotExist:
+            return None
+
+    def has_object_permission(self, request, post):
+        if post and (str(post.user.id) == str(request.user.id) or request.user.is_superuser):
+            return True
+        return False
+
+
+class PostDeleteProv(DestroyAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     permission_classes = ()
     lookup_field = 'id'
 
 
-class PostDeleteProv(DestroyAPIView):
+class PostList(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    permission_classes = ()
+
+# Delete
+# Testing the CRUD without Token
+class PostCreate(CreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = ()
+
+# Replace by RetrieveAPIView
+# Testing the CRUD without Token
+class PostDetail(RetrieveUpdateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = ()
+    lookup_field = 'id'
+
+# Delete
+# Testing the CRUD without Token
+class PostDelete(DestroyAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     permission_classes = ()
